2014faaf38ab85867746b7d6688310cf0e3cff87
Filipe Peixoto <filipenos@gmail.com>
2013-05-14 22:35:49 -0300

    Primeiro comando utilizando rails. :)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsBlog::Application.config.secret_token = 'c7c1546bdc6833e2ec12bd815f8203c0b2b0068041cc932ae64f844c741bff70718343cd344357c3467d7af5d8c7fe9476a92ec9cf343b52ac458c5f257a7433'
